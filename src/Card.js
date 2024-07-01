3a318843a10e8a12a349a3a6436dc3a8d7d8b8cf
Harsh Mittal <harsh@example.com>
2024-07-01 11:21:36 +0530

    add show more and less functaionality

--- src/Card.js
+++ src/Card.js
@@ -1,48 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 const Card = (props) => {
   const { post, onEdit, onDelete } = props;
-  const buttonStyles = {
-    margin: "5px", // Adjust spacing around buttons
+  const [isExpanded, setIsExpanded] = useState(false);
+  const toggleReadMore = () => {
+    setIsExpanded(!isExpanded);
   };
+  
   return (
-    /*
-    <div>
-        
-      <div className="card">
-        <div className="card-header">{post.title.toUpperCase()}</div>
-        <div className="card-body">
-          <h5 className="card-title">{post.body}</h5>
-         
-          
-          <div style={{ textAlign: "center" }}>
-          <button
-            onClick={() => onEdit(post)}
-            className="btn btn-secondary"
-            style={buttonStyles}
-          >
-            Edit
-          </button>
-          <button
-            onClick={() => onDelete(post.id)}
-            className="btn btn-danger"
-            style={buttonStyles}
-          >
-            Delete
-          </button>
-        </div>
-        </div>
-      </div>
-    </div>*/
+    
+    
     <div className="card" >
       <div className="card-body">
         <h5 className="card-title">{post.title.toUpperCase()}</h5>
-   
-        <p className="card-text">
-        {post.body}
-        </p>
+        {isExpanded? (<>
+      <p className="card-text">{post.body}</p>
+      <span className="text-primary" style={{ cursor: 'pointer' }} onClick={toggleReadMore}>
+        Show Less
+      </span>
+      </>
+    ):(<>
+    <p className="card-text">
+        {post.body.split("\n").slice(0, 1).join("\n")}...</p>
+        <span className="text-primary" style={{ cursor: 'pointer' }} onClick={toggleReadMore}>
+          Read More
+        </span>
+        </>)}
+
+        
         <div className="d-flex justify-content-center">
                   <i className="fas fa-edit text-primary mr-3 mx-2" style={{cursor: 'pointer', fontSize: '2.5em'}} onClick={() => onEdit(post)}></i>
                   <i className="fas fa-trash-alt text-danger mx-2" style={{cursor: 'pointer', fontSize: '2.5em'}} onClick={() => onDelete(post.id)}></i>
@@ -50,6 +37,8 @@ const Card = (props) => {
        
       </div>
     </div>
+    
+  
   );
 };
 
